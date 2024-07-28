a66632825667bc0645ecd3dfb79d125a0b00464f
TillWege <tillwege@gmail.com>
2024-07-28 12:33:59 +0200

    Add WGPU

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <webgpu/webgpu.h>
+#include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // We create a descriptor
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain = NULL;
+
+    // We create the instance using this descriptor
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+
+    // We can check whether there is actually an instance created
+    if (!instance) {
+        std::cerr << "Could not initialize WebGPU!" << std::endl;
+        return 1;
+    }
+
+    // Display the object (WGPUInstance is a simple pointer, it may be
+    // copied around without worrying about its size).
+    std::cout << "WGPU instance: " << instance << std::endl;
+
+
     return 0;
 }
